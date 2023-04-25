6d295c976c04c9f200bd7960c4b44625633db8d1
t-448 <i@tboiko.ru>
2023-04-26 01:30:03 +0300

    new commit

--- App/app.py
+++ App/app.py
@@ -1,7 +1,5 @@
 import flask
-from flask import render_template
-import pickle
-import numpy as np
+from flask import Flask, request, render_template
 import tensorflow as tf
 from tensorflow import keras
 
@@ -13,19 +11,18 @@ def prediction(params):
     return pred
 
 @app.route('/', methods=['POST', 'GET'])
-def predict():
+def main():
     if flask.request.method == 'GET':
         return render_template('main.html')
     
     if flask.request.method == 'POST':
-        params_list= float(flask.request.form['plotnost', 'modul_up', 'kolvo_otverd', 'epoxs_group', 'temperature', 
+        model = tf.keras.models.load_model('../models/model2/')
+        params = float(flask.request.form['plotnost', 'modul_up', 'kolvo_otverd', 'epoxs_group', 'temperature', 
                                               'poverh_plotnost', 'modul_up_rast', 'prochnost_rast', 'potreb_smol', 'ugol_nashivki', 
-                                              'shag_nashivki', 'plotnost_nashivki'])
-        params = params_list
-        params_list = params.reshape(1,-1)
-        result = params.append(float(params))
-        
-    return render_template('main.html', result=result)
+                                              'shag_nashivki', 'plotnost_nashivki'])   
+        params = params.reshape(1,-1)
+        result = model.predict([params])
+    return render_template('templates/main.html', result=result)
 
 if __name__ == '__main__':
     app.run()
