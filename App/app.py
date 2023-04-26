5c890d2aae7b0fdad6f51fc9244bed272430a72a
t-448 <i@tboiko.ru>
2023-04-26 13:53:20 +0300

    working app

--- App/app.py
+++ App/app.py
@@ -1,28 +1,31 @@
 import flask
-from flask import Flask, request, render_template
+from flask import render_template
 import tensorflow as tf
 from tensorflow import keras
+import numpy as np
 
 app = flask.Flask(__name__, template_folder = 'templates')
 
-def prediction(params):
-    model = pickle.load('models/<model-name>')
-    pred = model.predict([params])
-    return pred
+def prediction(param1, param2, param3, param4, param5, param6, param7, param8, param9, param10, param11, param12):
+    model = tf.keras.models.load_model('../models/model2/')
+    prediction = model.predict([param1, param2, param3, param4, param5, param6, param7, param8, param9, param10, param11, param12])
+    return prediction[0][0]
 
 @app.route('/', methods=['POST', 'GET'])
+
 def main():
+    params_list = []
+    result = ''    
     if flask.request.method == 'GET':
         return render_template('main.html')
     
     if flask.request.method == 'POST':
-        model = tf.keras.models.load_model('../models/model2/')
-        params = float(flask.request.form['plotnost', 'modul_up', 'kolvo_otverd', 'epoxs_group', 'temperature', 
-                                              'poverh_plotnost', 'modul_up_rast', 'prochnost_rast', 'potreb_smol', 'ugol_nashivki', 
-                                              'shag_nashivki', 'plotnost_nashivki'])   
-        params = params.reshape(1,-1)
-        result = model.predict([params])
-    return render_template('templates/main.html', result=result)
+        for i in range(1,13,1):
+            param = flask.request.form.get(f'param{i}')
+            params_list.append(float(param))
+            
+        result = prediction(*params_list)
+    return render_template('main.html', result=result)
 
 if __name__ == '__main__':
     app.run()
